9f15db5453fa48f79807b2c9c7cb6eabe6521c40
JARS3N <JARS3N@users.noreply.github.com>
2017-04-26 16:13:10 -0400

    Update server.R

--- inst/enterOligo/server.R
+++ inst/enterOligo/server.R
@@ -1,6 +1,9 @@
 library(shiny)
 shinyServer(function(input, output) {
    observeEvent(input$GO,{
+     if(input$cb==TRUE){
+        regQC::Outandsave(input$GO)
+        }
      if(!is.null(input$GO)){
      regQC::runOligoFolder()
      }
