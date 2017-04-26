49c58b85b98389dc6a0a62be3be51997f8000f63
JARS3N <JARS3N@users.noreply.github.com>
2017-04-26 16:02:21 -0400

    Update ui.R

--- inst/enterOligo/ui.R
+++ inst/enterOligo/ui.R
@@ -2,6 +2,7 @@ library(shiny)
 shinyUI(fluidPage(
   titlePanel("Oigo Analysis"),
     mainPanel(
+      shiny::checkboxInput('cb','export from wave to excel',value=FALSE),
       shiny::actionButton('GO','Select Directory'),
       p('Select Directory of Oligo Excel files to run analysis')
     )
