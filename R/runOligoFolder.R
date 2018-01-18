f5d262da86fb8b04e619c43cdd15f6495b5c307b
JARS3N <JARS3N@users.noreply.github.com>
2018-01-18 13:55:26 -0500

    Update runOligoFolder.R

--- R/runOligoFolder.R
+++ R/runOligoFolder.R
@@ -1,10 +1,20 @@
-runOligoFolder<-function(D){
-  setwd(D)
-  shh<-lapply(list.files(path=D,pattern='[.xls]$'),oligoCSV)
-  file.copy(system.file(package = 'regQC',"oligoIC50template.pzf"),D)
+runOligoFolder <- function(D) {
+  shh <-
+    lapply(list.files(
+      path = D,
+      pattern = '[.xlsx]$',
+      full.names = T
+    ),
+    oligoCSV)
+  file.copy(system.file(package = 'regQC', "oligoIC50template.pzf"), D)
   depositPZC(D)
-  shell.exec('oligoScript.PZC')
-  Sys.sleep(3)
-  system2("taskkill",args=c("/f","/im",'prism.exe'))
+  pzc <- file.path(D, 'oligoScript.PZC')
+  shell.exec(pzc)
+  Sys.sleep(2)
+  system2("taskkill", args = c("/f", "/im", 'prism.exe'))
+  fix_csv(list.files(
+    pattern = "csv$",
+    full.names = T,
+    path = D
+  ))
 }
-
